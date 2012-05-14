4a1ad23509c7eb85160596bc3035dcb657011cd1
tntgs <tnt@tnt.gs>
2012-05-14 16:44:48 -0500

    Testing

--- ZombieSurvival/src/gs/tnt/dev/minecraft/ZombieSurvival/Variables.java
+++ ZombieSurvival/src/gs/tnt/dev/minecraft/ZombieSurvival/Variables.java
@@ -10,11 +10,11 @@ package gs.tnt.dev.minecraft.ZombieSurvival;
 public final class Variables
 {
 	public static final String pluginName = "ZombieSurvival";
-	public static final String pluginVersion = "0.1.1.0";
+	public static final String pluginVersion = "0.1.1.1";
 	public static final byte pluginVersionMajor = 0;
 	public static final byte pluginVersionMinor = 1;
 	public static final byte pluginVersionBuild = 1;
-	public static final byte pluginVersionRevision = 0;
+	public static final byte pluginVersionRevision = 1;
 	
 	public static final short defaultPerWorldEntityLimit = 2000;
 	public static final short defaultPerWorldZombieLimit = 800;
